6a959a7a19d4e04235119b3aac04f922d59a1801
Gabriel-Costa-Silva <gabrielcostasilva.1918@gmail.com>
2024-04-24 17:23:30 -0300

    Alterações de importação de button dentro dos componentes dos módulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardRoxoComponent } from './card-roxo/card-roxo.component';
 import { CardComponent } from './card/card.component';
+import { CardButtonComponent } from './card-button/card-button.component';
+import { CardModule } from './card/card.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CardRoxoComponent,
-    CardComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CardModule
   ],
   providers: [
     provideClientHydration()
